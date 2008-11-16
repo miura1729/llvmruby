e607c6585a6c0aaefeb89feeb1cd37c8f48ecfb8
Arto Bendiken <arto@bendiken.net>
2008-11-16 08:07:54 +0100

    Fixed linker flags and implemented a workaround for an LLVM 2.4 bug in llvm-config that was causing dlopen(ext/llvmruby.bundle) to fail due to undefined symbols such as _OutputFilename.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -8,7 +8,13 @@ dir_config('llvm', `llvm-config --includedir`.strip, `llvm-config --libdir`.stri
 have_library('stdc++')
 have_library('pthread')
 
-with_ldflags(`llvm-config --libs all`) do 
+# This is a workaround for a dependency bug in llvm-config. See:
+# <https://mail.gna.org/public/etoile-discuss/2008-10/msg00014.html>
+# <https://mail.gna.org/public/etoile-discuss/2008-10/msg00015.html>
+# <http://svn.gna.org/viewcvs/etoile?rev=3694&view=rev>
+components = `llvm-config --libs all`.strip
+components = components.split(/\s+/).reject { |lib| lib =~ /LLVMCBase/ }.join(' ')
+
+with_ldflags([`llvm-config --ldflags`.strip, components].join(' ').strip) do
   create_makefile(extension_name)
 end
-
